e7ebbcfabda40b7e4610930a4f089f641d3e75f8
yashbeer <arjunsinghy157@gmail.com>
2022-01-03 18:08:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+
+import Resturant from "./Component/Basics/Resturant";
+const App = () => {
+  
+  return <Resturant />;
+};
+
+export default App;
